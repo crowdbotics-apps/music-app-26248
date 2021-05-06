d9009641fbb77a15c29882c9c5100dd935437826
Crowdbotics <team@crowdbotics.com>
2021-05-06 12:04:35 +0000

    Update models

--- backend/music_app_26248/wsgi.py
+++ backend/music_app_26248/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'music_app_26248.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "music_app_26248.settings")
 
 application = get_wsgi_application()
